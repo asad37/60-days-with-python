4e04b2dbb1004fbff9bf2b1df72bd2e15c60e6c7
asad37 <asadkambo2022@gmail.com>
2025-10-24 16:25:34 +0500

    add day23

---
+++ day23.py
@@ -0,0 +1,44 @@
+#python is very easy syntax language. it is used to create backend of Websites, to connect databases and also used to beautiful mobile applications.Python was introduce in 1991 and later on its popularity increase time by time.It was most famous till chat gpt backend sponsership.So Python is very easy to learn and its libraries which are pre-built make it more flexible and it create creousity in learner to learn more and more.It introduced Django fremework which is used to design powerful backend for websites and apps moreever its prebuilt modules are also so helpful in building amazing things.So finallly the result is that i like python very much and want to make my future in it.
+# print("Asad Waqas")
+# a=input("Enter a number to add:")
+# a=int(a)
+# b=input("Enter Second Number to add:")
+# b=int(b)
+# add=a+b
+# print(f"The sum of {a} and {b} is={add}")
+# if a>b:
+#     print(f"{a} is greater than {b}")
+# elif b>a:
+#     print(f"{b} is greater than {a}")
+# elif a==b:
+#     print(f"Ohhhh! {a} is equal to {b}")
+# else:
+#     print(f"invalid entries")
+# for i in range(a,b):
+#     print(f"{i}x{b}={i*b}")
+# context={
+#     "Name":"Asad Waqas",
+#     "age":23,
+#     "roll_number":12
+# }
+# context["Name"]="Ahmad Waqas"
+# context["age"]=25
+# print(context)
+
+# tuple_prac=("asad Waqas",23,"Ahmad")
+# print(tuple_prac)
+
+# list_practice=[1,2,3,4,5,5,6,6,6,"Asad Waqas",True]
+# list_practice[0]=12
+# list_practice.append(12)
+# for i in list_practice:
+#     print(i)
+# print(list_practice)
+
+
+# a=input("Enter a number:")
+# a=int(a)
+# for i in range(a+1):
+#     if i%2==0:
+#         print(i)
+print("\a")
